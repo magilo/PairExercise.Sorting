22750192005b80ee1c25cd8c7b62a309dbc7d053
magilo <53962625+magilo@users.noreply.github.com>
2019-10-22 12:11:39 -0400

    added some tests

---
+++ bubblesort.spec.js
@@ -0,0 +1,23 @@
+describe('Bubble Sort', function () {
+  it('handles an empty array', function () {
+    expect(bubbleSort([])).toEqual([]);
+  });
+
+  it('handles array of one number', function () {
+    expect(bubbleSort([-1])).toEqual([-1])
+  })
+
+  it('handles array of same numbers', function () {
+    expect(bubbleSort([0, 0, 0])).toEqual([0, 0, 0])
+  })
+
+  it('returns array of already sorted numbers in same order', function () {
+    expect(bubbleSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+    expect(bubbleSort([-7, 42, 100])).toEqual([-7, 42, 100])
+  })
+
+  it('handles array of unsorted numbers', function () {
+    expect(bubbleSort([100, 38, -100, -4, 7, 10])).toEqual([-100, -4, 7, 10, 38, 100])
+  })
+});
+
